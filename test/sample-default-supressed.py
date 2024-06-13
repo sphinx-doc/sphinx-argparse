6c4d34c293aa7e9fffab6ef127db37c52cf37366
Michele Riva <riva@iap.tuwien.ac.at>
2024-06-13 11:45:32 +0200

    more black formatting

--- test/sample-default-supressed.py
+++ test/sample-default-supressed.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 
+
 def get_parser():
     parser = ArgumentParser(prog='sample-default-suppressed', description='Test suppression of version default')
     parser.add_argument('--version', help='print version number', action='version', version='1.2.3')
