d62c5c2568811ff26a34ef35771c9454783cdcf4
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-07-15 15:22:17 +0100

    Enable PEP 583 annotations

--- sphinxarg/markdown.py
+++ sphinxarg/markdown.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 try:
     from commonmark import Parser
 except ImportError:
--- sphinxarg/parser.py
+++ sphinxarg/parser.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import re
 from argparse import _HelpAction, _StoreConstAction, _SubParsersAction
 
